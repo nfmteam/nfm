4d41a6631f9ea62d7f010c5d6f679defa776d167
keenwon <semanwmj@yeah.net>
2016-06-10 19:19:56 +0800

    eslint

---
+++ test/eslint.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const lint = require('mocha-eslint');
+
+var paths = [
+    'assets/**/*.js',
+    '!assets/app.*.js',
+    'controllers',
+    'lib',
+    '*.js'
+];
+
+var options = {
+    formatter: 'compact',
+    alwaysWarn: false,
+    timeout: 10000,
+    slow: 1000,
+    strict: true
+};
+
+lint(paths, options);
