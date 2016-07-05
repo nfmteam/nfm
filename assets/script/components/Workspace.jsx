af26b3c274afa0921ed5f958fe0ca7c8a9427552
keenwon <semanwmj@yeah.net>
2016-07-05 12:48:16 +0800

    优化path，增加面包屑导航

--- assets/script/components/Workspace.jsx
+++ assets/script/components/Workspace.jsx
@@ -1,5 +1,56 @@
 import React, { Component } from 'react';
 
+class Nav extends Component {
+
+    formatPath(path) {
+        let navPath, navName, index = 0, objs = [{
+            navPath: '/',
+            navName: '/'
+        }];
+
+        if (path === '/') {
+            return objs;
+        }
+
+        let paths = path.substr(1).split('/');
+        for (let i = 0, j = paths.length; i < j; i++) {
+            navName = paths[i];
+            index = path.indexOf(navName, index) + navName.length;
+            navPath = path.slice(0, index);
+
+            objs.push({
+                navPath,
+                navName
+            });
+        }
+
+        return objs;
+    }
+
+    render() {
+        const { path, clickHandler } = this.props;
+        const paths = this.formatPath(path);
+        const lastIndex = paths.length - 1;
+
+        return (
+            <p>
+                {
+                    paths.map((obj, index) => {
+                        return (
+                            <span key={obj.navPath}>
+                                <a href='javascript:;' onClick={() => clickHandler(obj.navPath)}>
+                                    {obj.navName}
+                                </a>
+                                {lastIndex !== index ? <i> > </i> : null}
+                            </span>
+                        )
+                    })
+                }
+            </p>
+        );
+    }
+}
+
 class Workspace extends Component {
 
     componentWillMount() {
@@ -23,6 +74,7 @@ class Workspace extends Component {
 
         return (
             <div style={{width: 700, position: 'absolute', top: 120, left: 460 }}>
+                <Nav path={currentPath} clickHandler={loadWorkspaceFilesHandler}/>
                 {
                     data.map(item => {
                         return (
