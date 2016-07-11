328f9ff68951759e9d63aecd9e7130940f950cb4
keenwon <semanwmj@yeah.net>
2016-07-11 22:41:48 +0800

    修改参数传递方式

--- controllers/api/v1/upload.js
+++ controllers/api/v1/upload.js
@@ -4,8 +4,15 @@ const fsHelper = require('../../../utils/fsHelper');
 const uploader = require('../../../utils/uploader');
 
 module.exports = function *() {
-    const { path } = this.params;
-    var uploadDir, formData, files, paths = [];
+    var path, uploadDir, formData, files, paths = [];
+
+    formData = yield uploader(this)
+        .catch(error => {
+            throw error;
+        });
+
+    path = formData.fields.path;
+    files = formData.files.files;
 
     if (!path) {
         throw Error('入参错误');
@@ -16,14 +23,6 @@ module.exports = function *() {
     }
 
     uploadDir = fsHelper.resolveAbsolutePath(path);
-
-    // 错误处理
-    formData = yield uploader(this)
-        .catch(error => {
-            throw error;
-        });
-
-    files = formData.files.files;
 
     if (!files) {
         throw Error('files字段为空');
@@ -36,7 +35,12 @@ module.exports = function *() {
     // 移动文件到path
     for (let file of files) {
         paths.push(`${uploadDir}/${file.name}`);
-        yield fsHelper.fs.moveAsync(file.path, `${uploadDir}/${file.name}`)
+
+        // 重复则覆盖文件
+        yield fsHelper.fs.moveAsync(file.path, `${uploadDir}/${file.name}`, { clobber: true })
+            .catch(error => {
+                throw error;
+            });
     }
 
     // 返回上传的文件列表
--- test/controllers/api/v1/upload.spec.js
+++ test/controllers/api/v1/upload.spec.js
@@ -29,22 +29,16 @@ const filesPath = path.resolve(__dirname, '../../../files');
 
 describe('fs upload测试', function () {
 
-    before('before upload测试', function () {
+    beforeEach(function () {
         fs.ensureDirSync(basePath);
         fs.ensureDirSync(`${basePath}/${config['upload.dir']}`);
-    });
 
-    after('after upload测试', function () {
-        fs.removeSync(basePath);
-    });
-
-    beforeEach(function () {
         var app = koa();
 
         app.use(bodyParser);
 
-        router.all('/api/*', apiParser);
-        router.post('/api/:path', uploadApi);
+        router.all('*', apiParser);
+        router.post('/api/upload', uploadApi);
 
         app.use(router.routes());
 
@@ -53,17 +47,36 @@ describe('fs upload测试', function () {
 
     afterEach(function () {
         this.server.close();
+
+        fs.removeSync(basePath);
     });
 
-    it('# 上传文件', function (done) {
+    it('# 单文件上传', function (done) {
         var uploadDir = '/',
             form = new FormData(),
             headers = form.getHeaders();
 
+        form.append('path', uploadDir);
+        form.append('files', fs.createReadStream(`${filesPath}/lib/subdomain.js`));
+
+        upload(`http://localhost:8888/api/upload`, form, headers)
+            .then(() => {
+                if (fs.existsSync(path.join(basePath, 'subdomain.js'))) {
+                    done();
+                }
+            });
+    });
+
+    it('# 多文件上传', function (done) {
+        var uploadDir = '/',
+            form = new FormData(),
+            headers = form.getHeaders();
+
+        form.append('path', uploadDir);
         form.append('files', fs.createReadStream(`${filesPath}/README.md`));
         form.append('files', fs.createReadStream(`${filesPath}/package.json`));
 
-        upload(`http://localhost:8888/api/${encodeURIComponent(uploadDir)}`, form, headers)
+        upload(`http://localhost:8888/api/upload`, form, headers)
             .then(() => {
                 if (fs.existsSync(path.join(basePath, 'README.md'))
                     && fs.existsSync(path.join(basePath, 'package.json'))) {
@@ -71,5 +84,70 @@ describe('fs upload测试', function () {
                 }
             });
     });
+
+    it('# 入参错误:未传path', function (done) {
+        var form = new FormData(),
+            headers = form.getHeaders();
+
+        upload(`http://localhost:8888/api/upload`, form, headers)
+            .then(response => {
+                response.message.should.equal('入参错误');
+                done();
+            });
+    });
+
+    it('# 入参错误:path不存在', function (done) {
+        var uploadDir = '/keenwon',
+            form = new FormData(),
+            headers = form.getHeaders();
+
+        form.append('path', uploadDir);
+
+        upload(`http://localhost:8888/api/upload`, form, headers)
+            .then(response => {
+                response.message.should.equal('路径不存在');
+                done();
+            });
+    });
+
+    it('# 入参错误:files不存在', function (done) {
+        var uploadDir = '/',
+            form = new FormData(),
+            headers = form.getHeaders();
+
+        form.append('path', uploadDir);
+
+        upload(`http://localhost:8888/api/upload`, form, headers)
+            .then(response => {
+                response.message.should.equal('files字段为空');
+                done();
+            });
+    });
+
+    it('# 文件已存在', function (done) {
+        var uploadDir = '/',
+            form1 = new FormData(),
+            headers1 = form1.getHeaders();
+
+        form1.append('path', uploadDir);
+        form1.append('files', fs.createReadStream(`${filesPath}/README.md`));
+
+        upload(`http://localhost:8888/api/upload`, form1, headers1)
+            .then(() => {
+
+                var form2 = new FormData(),
+                    headers2 = form2.getHeaders();
+
+                form2.append('path', uploadDir);
+                form2.append('files', fs.createReadStream(`${filesPath}/README.md`));
+
+                upload(`http://localhost:8888/api/upload`, form2, headers2)
+                    .then(() => {
+                        done();
+                    })
+            });
+    });
+
+    
 
 });
